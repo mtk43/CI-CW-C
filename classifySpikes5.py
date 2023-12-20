7dec903d1d1e15a5d55c385bc741d1f2e106c635
Max Kienzle <mtk43@bath.ac.uk>
2023-12-20 04:31:44 +0100

    CNN code trained and tested on filtered data

--- classifySpikes5.py
+++ classifySpikes5.py
@@ -9,6 +9,16 @@ import numpy as np
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import itertools
+from scipy.signal import butter, filtfilt, find_peaks
+import statistics
+
+# Define function to create a low-pass Butterworth filter
+def butter_lowpass_filter(data, cutoff_frequency, sampling_rate, order=4):
+    nyquist = 0.5 * sampling_rate
+    normal_cutoff = cutoff_frequency / nyquist
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    y = filtfilt(b, a, data)
+    return y
 
 # Define the convolutional neural network model
 class ConvolutionalNetwork(nn.Module):
@@ -33,10 +43,12 @@ class ConvolutionalNetwork(nn.Module):
 # Define function to train the network
 def train_network(d, indices, Class, window_shift, window_size):
     for i in range(len(indices)):
+        window_original = np.arange(indices[i] - window_size//2, indices[i] + window_size//2)
         for shift in window_shift:
-            start_index = indices[i] + shift
-            end_index = start_index + window_size
-            window = range(start_index, end_index)
+            #start_index = indices[i] + shift
+            #end_index = start_index + window_size
+            #window = range(start_index, end_index)
+            window = np.roll(window_original, shift)
 
             target_class = Class[i]
             target_values = torch.tensor(target_class - 1, dtype=torch.long)
@@ -56,7 +68,8 @@ def test_network(d, Index_test, Class_test, window_size):
     scorecard = []
     predicted_classes = []
     for i in range(len(Index_test)):
-        window = range(Index_test[i] - 10, Index_test[i] + 40)
+        #window = range(Index_test[i] - 10, Index_test[i] + 40)
+        window = range(Index_test[i] - window_size//2, Index_test[i] + window_size//2)
         x_test_tensor = torch.tensor(d[window], dtype=torch.float32).unsqueeze(0).unsqueeze(0)
         #y_test_tensor = torch.tensor(Class_test[i] - 1, dtype=torch.long)
 
@@ -106,6 +119,35 @@ sorted_indices = np.argsort(Index)
 Index = Index[sorted_indices]
 Class = Class[sorted_indices]
 
+# Low-pass filter parameters
+sampling_rate = 25000  # sampling rate (Hz)
+cutoff_frequency = 1200  
+
+# Apply the low-pass filter to the test signal
+d_filt = butter_lowpass_filter(d, cutoff_frequency, sampling_rate)
+
+# Calculate and print the standard deviation of 'd'
+std_deviation = statistics.stdev(d_filt)
+
+# Find peaks in the filtered signal
+peaks, _ = find_peaks(d_filt, prominence=std_deviation)
+
+index_filt = np.zeros(len(peaks), dtype=int)
+class_filt = np.zeros(len(peaks), dtype=int)
+# Find the classes corresponding to the filtered peaks
+for i in range(len(peaks)):
+    # Get the value of the current peak
+    peak_value = d[peaks[i]]  
+    
+    # Find the closest Index value to the current peak
+    closest_index = min(Index, key=lambda x: abs(x - peaks[i]))
+
+    # Find the class value associated with the closest_index
+    class_value = Class[np.where(Index == closest_index)][0]
+
+    index_filt[i] = round(closest_index)
+    class_filt[i] = round(class_value)
+
 Index_train = Index[:round(len(Index) * 0.8)]
 Class_train = Class[:round(len(Class) * 0.8)]
 
@@ -113,15 +155,18 @@ Index_test = Index[round(len(Index) * 0.8) + 1:]
 Class_test = Class[round(len(Class) * 0.8) + 1:]
 
 window_size = 50
+learning_rate = 0.01
 
 model = ConvolutionalNetwork()
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.01)
+optimizer = optim.SGD(model.parameters(), learning_rate)
 
-window_shift = [-10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10]
+#window_shift = [-10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10]
+window_shift = list(range(-10, 10))
 
 # Train the network
 train_network(d, Index_train, Class_train, window_shift, window_size)
+
 
 # Test the network
 scorecard, predicted_classes = test_network(d, Index_test, Class_test, window_size)
@@ -135,6 +180,7 @@ print(f"Performance: {performance:.2f}%")
 
 # Create confusion matrix
 cm = confusion_matrix(Class_test, predicted_classes)
+#cm = confusion_matrix(class_test_filt, predicted_classes)
 
 # Plot confusion matrix
 class_names = [str(i) for i in range(1, 6)]
